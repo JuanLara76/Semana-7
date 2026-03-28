df6dd036f8a5ee432e9837cd636ef1e3136c3fca
Juan Lara <juandavidlara2005@gmail.com>
2026-03-27 21:23:42 -0500

    Usuario y contraseña.py

---
+++ Usuario y contraseña.py
@@ -0,0 +1,7 @@
+Usuario=input("Ingrese usuario:")
+Contraseña=int(input("Ingrese contraseña:"))
+if Usuario == "Admin" and Contraseña== 1234:
+  print("Caragando tu perfil...")
+
+else:
+  print("Acceso denegado, datos incorrectos")
